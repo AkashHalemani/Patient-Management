43fec6e488afcffb2ff5788dbf209d80797c28bc
Akash K Halemani <akashhalemani123@gmail.com>
2025-11-13 19:10:42 +0530

    Commit till analytics-service. To start API Gateways

---
+++ patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -0,0 +1,29 @@
+package com.pm.patient_service.mapper;
+
+import com.pm.patient_service.dto.PatientRequestDTO;
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.module.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
+        patientResponseDTO.setId(patient.getId().toString());
+        patientResponseDTO.setAddress(patient.getAddress());
+        patientResponseDTO.setName(patient.getName());
+        patientResponseDTO.setEmail(patient.getEmail());
+        patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        return patientResponseDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setName(patientRequestDTO.getName());
+        return  patient;
+    }
+}
